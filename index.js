7e0d33d87d47d4b0c97eecc40bbaeaa0e5483b6c
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-11-17 01:27:43 +0600

    server loaded appointment options

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
-const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: "Unauthorized access" });
-  }
-  const token = authHeader.split(" ").pop();
-  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-    if (err) {
-      return res.status(403).send({ message: "Forbidden access" });
-    }
-    req.decoded = decoded;
-    next();
-  });
-};
+// const verifyToken = (req, res, next) => {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader) {
+//     return res.status(401).send({ message: "Unauthorized access" });
+//   }
+//   const token = authHeader.split(" ").pop();
+//   jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+//     if (err) {
+//       return res.status(403).send({ message: "Forbidden access" });
+//     }
+//     req.decoded = decoded;
+//     next();
+//   });
+// };
 
 // localhost server setup
 const server = app.listen(port, "localhost", () => {
@@ -52,16 +52,25 @@ const run = async () => {
   try {
     const database = client.db("doctorsPortal");
 
-    app.post("/jwt", async (req, res) => {
-      const userEmail = req.body;
-      const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN, {
-        expiresIn: "10h"
-      });
-      res.send({ token });
-    });
+    // app.post("/jwt", async (req, res) => {
+    //   const userEmail = req.body;
+    //   const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN, {
+    //     expiresIn: "10h"
+    //   });
+    //   res.send({ token });
+    // });
 
     const appointmentOptionsCollection =
       database.collection("appointmentOptions");
+
+    app.get("/appointmentOptions", async (req, res) => {
+      const query = {};
+      const options = {};
+      const result = await appointmentOptionsCollection
+        .find(query, options)
+        .toArray();
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
